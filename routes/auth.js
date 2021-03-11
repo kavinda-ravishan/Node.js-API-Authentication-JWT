a135e97b172fa1c59067d6681d645b8e62929975
kavinda ravishan <kvnravishan@outlook.com>
2021-03-11 14:49:57 +0530

    login form

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,41 @@ const { registrValidation, loginValidation } = require("../validation");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+//Register the user Page
+router.get("/login", async (req, res) => {
+  const path = __dirname.replace("routes", "");
+  res.sendFile(`${path}client/login.html`);
+});
+
+//login
+router.post("/login", async (req, res) => {
+  //Valiate the data befor login the user
+
+  const { error } = loginValidation(req.body);
+  if (error) {
+    return res.status(400).header("error", error.details[0].message).end();
+  }
+
+  //Chech if the email exists
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return res.status(400).header("error", "Email dosnot Exists!").end();
+  }
+
+  //Now user is logged in//
+
+  //Match the password
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass)
+    return res.status(400).header("error", "Password invalid !").end();
+
+  //Craet and assigned a token
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+  res.setHeader("auth-token", token);
+  res.setHeader("name", user.name);
+  res.end();
+});
+
 //Register the user
 router.post("/register", async (req, res) => {
   //Valiate the data befor make a user
@@ -30,36 +65,10 @@ router.post("/register", async (req, res) => {
   });
   try {
     const saveUser = await user.save();
-    res.send({ user: user._id });
+    res.send({ user: saveUser._id });
   } catch (err) {
     res.status(400).send(err);
   }
-});
-
-//login
-router.post("/login", async (req, res) => {
-  //Valiate the data befor login the user
-
-  const { error } = loginValidation(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
-
-  //Chech if the email exists
-  const user = await User.findOne({ email: req.body.email });
-  if (!user) {
-    return res.status(400).send("Email dosnot Exists!");
-  }
-
-  //Now user is logged in//
-
-  //Match the password
-  const validPass = await bcrypt.compare(req.body.password, user.password);
-  if (!validPass) return res.status(400).send("Password invalid !");
-
-  //Craet and assigned a token
-  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
-  res.header("auth-token", token).send(token);
 });
 
 module.exports = router;
--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const verify = require("./verifyToken");
-const User = require("../model/User");
 
 router.get("/", verify, (req, res) => {
   //res.json({ posts: { title: "My post", description: "some data.." } });
